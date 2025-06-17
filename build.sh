53d3ce28325f455066a6bdcf256ef1477dff6d8f
Dex1232007 <sanmoe09472@gmail.com>
2025-06-18 02:47:51 +0630

    Update and rename build.sh to Procfile

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Install yt-dlp into project root
-
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
-chmod +x yt-dlp
-mv yt-dlp /usr/local/bin/
